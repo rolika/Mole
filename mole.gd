c694d761e39fd0651d468c787abf9b8149abb0bc
Weisz Roland <rolika@nostromo>
2023-09-23 17:24:28 +0200

    randomize moles

--- mole.gd
+++ mole.gd
@@ -2,11 +2,17 @@ extends Area2D
 
 
 var threatened: bool
+var hidetimer
+var dancetimer 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	threatened = false
+	hidetimer = $HideTimer
+	dancetimer = $DanceTimer
+	init_timer(hidetimer, 5)
+	init_timer(dancetimer, 3)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,4 +31,18 @@ func _on_area_exited(_area):
 func _on_player_smashed():
 	if threatened:
 		print("BANG!!!")
-		hide()
+		_on_dance_timer_timeout()
+
+
+func _on_hide_timer_timeout():
+	show()
+	init_timer(dancetimer, 3)
+
+
+func _on_dance_timer_timeout():
+	hide()	
+	init_timer(hidetimer, 5)
+
+func init_timer(timer:Timer, duration:int):
+	timer.wait_time = randi() % duration + 1
+	timer.start()
